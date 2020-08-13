64b6f696da7de1def4ff6ce5bf60d5ac40f445ea
David Yemaneab <intdzy@yahoo.com>
2020-08-13 17:03:21 -0500

    test

---
+++ build/precache-manifest.9f73940a023f455351d95f4da5a79fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a04cdbccc426ecdaf04675f08cb562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "453e224082435cd2bc8d",
+    "url": "/static/js/2.d30ff7af.chunk.js"
+  },
+  {
+    "revision": "e507ea3430490d9338d4",
+    "url": "/static/js/main.3be93a9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/static/media/logo.a0185b04.svg"
+  }
+]);
